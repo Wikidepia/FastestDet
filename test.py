066ca93c7ea243f299e982bb20ffdb17e53c6125
Akmal <magchuz@pm.me>
2024-02-24 20:09:20 +0700

    Add pad to square in test

--- test.py
+++ test.py
@@ -3,12 +3,59 @@ import os
 import time
 
 import cv2
+import numpy as np
 import onnx
 import torch
 from onnxsim import simplify
 
 from module.detector import Detector
 from utils.tool import *
+
+
+# https://stackoverflow.com/a/44659589
+def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    # return the resized image
+    return resized
+
+
+def pad_to_square(img, size, pad_value=0):
+    img = image_resize(img, width=size)
+    # pad to sizexsize
+    h, w, _ = img.shape
+    h_diff = size - h
+    w_diff = size - w
+    img = cv2.copyMakeBorder(
+        img, 0, h_diff, 0, w_diff, cv2.BORDER_CONSTANT, value=pad_value
+    )
+    return img
+
 
 if __name__ == "__main__":
     # 指定训练配置文件
@@ -60,9 +107,7 @@ if __name__ == "__main__":
 
     # 数据预处理
     ori_img = cv2.imread(opt.img)
-    res_img = cv2.resize(
-        ori_img, (cfg.input_width, cfg.input_height), interpolation=cv2.INTER_LINEAR
-    )
+    res_img = pad_to_square(ori_img, cfg.input_height)
     img = res_img.reshape(1, cfg.input_height, cfg.input_width, 3)
     img = torch.from_numpy(img.transpose(0, 3, 1, 2))
     img = img.to(device).float() / 255.0
